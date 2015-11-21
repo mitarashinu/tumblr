5893fb5fc15a3c14219af26ccdde1f5fb0b14a32
mtwtkman <punipuniomochi@gmail.com>
2015-11-21 17:12:39 +0900

    routing

--- planet/js/component/index.js
+++ planet/js/component/index.js
@@ -2,9 +2,16 @@ var m = require('mithril');
 
 module.exports = {
   controller: function() {
-    return {value: 'planet'};
+    return {
+      links: ['earth']
+    };
   },
   view: function(ctrl) {
-    return m('h1', ctrl.value);
+    return [
+      m('h1', {style: {textAlign: 'center'}}, 'planet'),
+      m('ul', ctrl.links.map(function(url) {
+        return m('li', m('a', {href: '/' + url, config: m.route}, 'earth'));
+      }))
+    ];
   }
 }
